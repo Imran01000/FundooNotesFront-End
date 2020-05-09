26d2f6f38a6b82299df9f27a7f08e84c04645a5f
Imran01000 <imshaikh01000@gmail.com>
2020-05-09 18:21:57 +0530

    MODIFIED:Added all functionality

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  formGroup: FormGroup;
+  alertMessageName: string = 'This field is required!!';
+  alertMessagePassword: string = 'Please set your password!!';
+  alertMessageConfirm: string = 'Please confirm your password!!';
+  alertMessageEmail: string = 'Email missing!!';
+  alertMessageContactNo: string = 'Please provide your contact!!';
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm() {
+       this.formGroup = this.formBuilder.group({
+      'firstname':[null,[Validators.required]],
+      'lastname':[null,[Validators.required]],
+      'email':[null,[Validators.required]],
+      'password':[null,[Validators.required]],
+      'confirmpassword':[null,[Validators.required]],
+      'contactno':[null,[Validators.required]]
+    });
   }
 
 }
