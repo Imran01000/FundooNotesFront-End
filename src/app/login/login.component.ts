0af362e805dd70fbd182bb593ab19ad2f4793966
Imran01000 <imshaikh01000@gmail.com>
2020-05-14 18:52:13 +0530

    MODIFIED:updated validation part and icon functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
+import { LoginService } from '../login.service';
+import { Login } from './login';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +11,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  alertMessageName: string = 'This field is required!!';
+  alertMessagePassword: string = 'Please provide password!!';
+  hide = true;
+  constructor(private service:LoginService , private snackBar:MatSnackBar) { }
 
-  constructor() { }
+  login: Login = new Login("","");
+  message:any;
+
+  form: FormGroup = new FormGroup({
+    email: new FormControl('',[Validators.required , Validators.email,]),
+    password: new FormControl('',[Validators.required , Validators.minLength(6),])
+  })
 
   ngOnInit(): void {
+    this.loginNow();
+  }
+  
+  public loginNow(){
+    let resp = this.service.doLogin(this.login);
+    resp.subscribe((data)=>this.message=data);
   }
 
+  public openSnackBar(message)
+  {
+    this.snackBar.open(message);
+  }
 }
+
+
+  
+
 
