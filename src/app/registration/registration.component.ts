6b3bb59a7d39465cbde24f8e307360d08a920995
Imran01000 <imshaikh01000@gmail.com>
2020-05-14 20:37:05 +0530

    MODIFIED:Added validation implementation and snackbar

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
+import { Registration } from './registration';
+import { RegistrationService } from '../registration.service';
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-registration',
@@ -8,28 +11,40 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-  formGroup: FormGroup;
   alertMessageName: string = 'This field is required!!';
   alertMessagePassword: string = 'Please set your password!!';
   alertMessageConfirm: string = 'Please confirm your password!!';
   alertMessageEmail: string = 'Email missing!!';
   alertMessageContactNo: string = 'Please provide your contact!!';
+  hide = true;
 
-  constructor(private formBuilder: FormBuilder) { }
+  form = new FormGroup({
+    firstName: new FormControl('',[Validators.required , Validators.pattern('[A-Z][a-z]*')]),
+    lastName: new FormControl('',[Validators.required , Validators.pattern('[A-Z][a-z]*')]),
+    email: new FormControl('',[Validators.required , Validators.email]),
+    password:  new FormControl('',[Validators.required , Validators.minLength(6)]),
+    confirmPassword: new FormControl('',Validators.required),
+    contactNo: new FormControl('',[Validators.required , Validators.pattern('[7-9][0-9]{9}')])
+  });
 
-  ngOnInit(): void {
-    this.createForm();
+  constructor(private service:RegistrationService , private snackBar:MatSnackBar) { }
+  registration: Registration = new Registration("","","","","");
+  message:any;
+
+  ngOnInit(): void 
+  {
+    this.registerNow();
+  }
+  
+  public registerNow()
+  {
+    let resp = this.service.doRegistration(this.registration);
+    resp.subscribe((data)=>this.message=data);
   }
 
-  createForm() {
-       this.formGroup = this.formBuilder.group({
-      'firstname':[null,[Validators.required]],
-      'lastname':[null,[Validators.required]],
-      'email':[null,[Validators.required]],
-      'password':[null,[Validators.required]],
-      'confirmpassword':[null,[Validators.required]],
-      'contactno':[null,[Validators.required]]
-    });
+  public openSnackBar(message)
+  {
+    this.snackBar.open(message);
   }
 
 }
