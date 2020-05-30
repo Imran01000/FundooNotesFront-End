354d183bd017f1141eb043bc6bf64548b7290492
Imran01000 <imshaikh01000@gmail.com>
2020-05-30 18:28:17 +0530

    ADDED:Added note service

---
+++ src/app/services/note.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Note } from '../dto/noteDto';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoteService {
+
+  constructor(private http:HttpClient) { }
+
+  private httpUrl: "http://localhost:8082/user";
+
+  headers = new HttpHeaders({'Content-Type' : 'application/json' , token: localStorage.getItem("token")});
+
+  addNote(addNote : Note): Observable<any>
+  {
+    console.log(this.headers);
+     return this.http.post("http://localhost:8082/user/addnote" , addNote , {headers : new HttpHeaders({ 'token' : localStorage.getItem("token")})});
+  }
+
+}
