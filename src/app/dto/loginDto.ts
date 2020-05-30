e686476fd776778568d6576a8308a4fb0e1f1b7f
Imran01000 <imshaikh01000@gmail.com>
2020-05-30 18:22:47 +0530

    MODIFIED:Removed constructor

--- src/app/dto/loginDto.ts
+++ src/app/dto/loginDto.ts
@@ -2,9 +2,4 @@ export class Login
 {
     email: string;
     password:string;
-    constructor(email:string , password:string)
-    {
-        this.email =  email;
-        this.password = password;
-    }
 }
