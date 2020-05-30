5b19d0e6d8dbddb7a9e313da522f341acb396127
Imran01000 <imshaikh01000@gmail.com>
2020-05-30 18:21:43 +0530

    MODIFIED:Removed constructor

--- src/app/dto/registrationDto.ts
+++ src/app/dto/registrationDto.ts
@@ -4,12 +4,4 @@ export class Registration{
     email: string;
     password: string;
     mobileNo: string;
-    constructor(fname: string, lname: string, email: string, password: string, mobileNo: string)
-    {
-       this.fname = fname;
-       this.lname = lname;
-       this.email = email;
-       this.password = password;
-       this.mobileNo = mobileNo;
-    }
 }
