0cf7c3114c8766a6f79ba5b07c871fd02ac3051a
Imran01000 <imshaikh01000@gmail.com>
2020-05-30 18:18:40 +0530

    MODIFIED:Modified some functionality

--- src/app/components/note/note.component.ts
+++ src/app/components/note/note.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NoteService } from 'src/app/services/note.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Note } from 'src/app/dto/noteDto';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-note',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: NoteService , private snackBar: MatSnackBar) { }
+  note: Note = new Note();
+  open:boolean = false;
+
+  title = new FormControl();
+  description = new FormControl();
 
   ngOnInit(): void {
   }
 
+  openNote(){
+    this.open = true;
+  }
+  closeNote(){
+    this.open = false;
+  }
+  addNoteNow(){
+    this.note.title = this.title.value;
+    this.note.description = this.description.value;
+    this.service.addNote(this.note).subscribe(
+      (response: any) => {
+        this.snackBar.open("Note added", "OK", { duration: 3000 });
+      },
+      (error: any) => {
+        this.snackBar.open("Note failed to add", "Something goes wrong", { duration: 3000 });
+      }
+    )
+  }
 }
