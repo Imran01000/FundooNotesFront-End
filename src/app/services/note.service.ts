2bf0467b627f7109670c9221be6eb2f8e564b0e8
Imran01000 <imshaikh01000@gmail.com>
2020-06-09 19:04:54 +0530

    MODIFIED:Added delete note and color note method

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Note } from '../dto/noteDto';
 import { Observable } from 'rxjs';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,14 +11,14 @@ export class NoteService {
 
   constructor(private http:HttpClient) { }
 
-  private httpUrl: "http://localhost:8082/user";
+  private httpUrl= "http://localhost:8082/user/";
 
   headers = new HttpHeaders({'Content-Type' : 'application/json' , token: localStorage.getItem("token")});
 
   addNote(addNote : Note): Observable<any>
   {
     console.log(this.headers);
-     return this.http.post("http://localhost:8082/user/addnote" , addNote , {headers : new HttpHeaders({ 'token' : localStorage.getItem("token")})});
+     return this.http.post(this.httpUrl+'addnote' , addNote , {headers : new HttpHeaders({ 'token' : localStorage.getItem("token")})});
   }
 
   getNotes(): Observable<any>
@@ -25,6 +26,35 @@ export class NoteService {
     console.log(this.headers);
      return this.http.get("http://localhost:8082/user/showall" ,{
       headers: new HttpHeaders().set("token", localStorage.getItem("token")),
+      observe: 'response'
+    });
+  }
+
+  setArchive(url , vary:any)
+  {
+    return this.http.put(this.httpUrl+url,vary,{
+      headers: new HttpHeaders().set("token", localStorage.getItem("token")),
+      observe: 'response'
+    });
+  } 
+
+  getRequestNote(url): Observable<any> {
+    return this.http.get(this.httpUrl + url, {
+      headers: new HttpHeaders().set("jwt_token", localStorage.getItem("token")),
+      observe: 'response'
+    });
+  }
+  
+   deleteNote(noteId: number) {
+    return this.http.delete(this.httpUrl + 'delete/' + noteId, {
+      headers: new HttpHeaders().set("jwt_token", localStorage.getItem("token")),
+      observe: 'response'
+    });
+  }
+
+  addColor(url, vary: any) {
+    return this.http.put(this.httpUrl + url, vary, {
+      headers: new HttpHeaders().set("jwt_token", localStorage.getItem("token")),
       observe: 'response'
     });
   }
