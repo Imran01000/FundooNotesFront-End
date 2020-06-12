9418d1450a18fa234d418f293171838bc15bfb33
Imran01000 <imshaikh01000@gmail.com>
2020-06-12 17:37:13 +0530

    MODIFIED:Added subject and tap

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Note } from '../dto/noteDto';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
 
 
 @Injectable({
@@ -14,6 +16,13 @@ export class NoteService {
   private httpUrl= "http://localhost:8082/user/";
 
   headers = new HttpHeaders({'Content-Type' : 'application/json' , token: localStorage.getItem("token")});
+  
+  autoRefresh$ = new Subject<void>();
+
+  get doRefreshAuto()
+  {
+    return this.autoRefresh$;
+  }
 
   addNote(addNote : Note): Observable<any>
   {
@@ -24,15 +33,25 @@ export class NoteService {
   getNotes(): Observable<any>
   {
     console.log(this.headers);
-     return this.http.get("http://localhost:8082/user/showall" ,{
+     return this.http.get(this.httpUrl+'showall',{
       headers: new HttpHeaders().set("token", localStorage.getItem("token")),
       observe: 'response'
     });
   }
 
-  setArchive(url , vary:any)
+  setArchive(url , vary:any):Observable<any> 
   {
     return this.http.put(this.httpUrl+url,vary,{
+      headers: new HttpHeaders().set("token", localStorage.getItem("token")),
+      observe: 'response'
+    }).pipe(
+      tap(()=>{this.autoRefresh$.next()})
+    );
+  } 
+
+  getArchiveNotes():Observable<any> 
+  {
+    return this.http.get("http://localhost:8082/user/showall",{
       headers: new HttpHeaders().set("token", localStorage.getItem("token")),
       observe: 'response'
     });
@@ -40,21 +59,21 @@ export class NoteService {
 
   getRequestNote(url): Observable<any> {
     return this.http.get(this.httpUrl + url, {
-      headers: new HttpHeaders().set("jwt_token", localStorage.getItem("token")),
-      observe: 'response'
-    });
-  }
-  
-   deleteNote(noteId: number) {
-    return this.http.delete(this.httpUrl + 'delete/' + noteId, {
-      headers: new HttpHeaders().set("jwt_token", localStorage.getItem("token")),
+      headers: new HttpHeaders().set("token", localStorage.getItem("token")),
       observe: 'response'
     });
   }
 
-  addColor(url, vary: any) {
+   deleteNote(noteId: number):Observable<any>  {
+    return this.http.delete(this.httpUrl + 'deletenote/' + noteId, {
+      headers: new HttpHeaders().set("token", localStorage.getItem("token")),
+      observe: 'response'
+    });
+  }
+
+  addColor(url, vary: any):Observable<any> {
     return this.http.put(this.httpUrl + url, vary, {
-      headers: new HttpHeaders().set("jwt_token", localStorage.getItem("token")),
+      headers: new HttpHeaders().set("token", localStorage.getItem("token")),
       observe: 'response'
     });
   }
