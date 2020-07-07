6b204cba1cd4a6c6e87da7130ac6dac34200cd8e
Imran01000 <imshaikh01000@gmail.com>
2020-07-07 21:31:23 +0530

    MODIFIED:Added some variable

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NoteService } from 'src/app/services/note.service';
+import { DataService } from 'src/app/services/data.service';
+import { MatDialog } from '@angular/material/dialog';
+import { EditLabelsComponent } from '../edit-labels/edit-labels.component';
+import { LabelServiceService } from 'src/app/services/label-service.service';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +14,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private noteService:NoteService, private router:Router, 
+    private dataService:DataService, private matDialog: MatDialog,
+    private labelService:LabelServiceService) { }
+    
   opened:boolean = false;
+  view: boolean = false;
+  grid = "row";
+  labels: any[];
   ngOnInit(): void {
+    this.getAllLabel()
   }
 
+  onArchive() {
+    this.router.navigate(['dashboard', 'archivenotes']);
+  }
+
+  onTrash()
+  {
+    this.router.navigate(['dashboard' , 'trashnotes']);
+  }
+
+  onReminder()
+  {
+    this.router.navigate(['dashboard' , 'reminder-notes']);
+  }
+
+  getView() {
+    if (this.view == true) {
+      this.view = false;
+      this.grid = "row";
+    }
+    else {
+      this.view = true;
+      this.grid = "column";
+    }
+    this.dataService.setView(this.grid);
+    console.log(this.view);
+  }
+
+  getAllLabel() {
+    this.labelService.getAllLabel('getall').subscribe(
+      response => {
+        console.log(response)
+        this.labels = response['body']
+        console.log("info===label==", this.labels)
+      },
+      error => {
+        console.log('Error', error);
+      }
+    )
+  }
+
+  openEditLabelDialog() {
+    const dialogRef = this.matDialog.open(EditLabelsComponent,
+      {
+        width: "300px",
+        height: "Auto"
+      }
+    );
+    dialogRef.afterClosed().subscribe(result => {
+      console.log("dialog closed");
+    });
+  }
 }
